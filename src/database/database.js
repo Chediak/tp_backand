5cce22ac2d081a69d1927dbf95ab0dabae601839
Edgar Santiago <edgargsp777@gmail.com>
2020-11-08 15:32:20 -0300

    rsrsss

--- src/database/database.js
+++ src/database/database.js
@@ -6,12 +6,27 @@ const environment = process.env.NODE_ENV || 'development';
 const config = require ('../config/config.js') [environment];
 
 msql.createConnection(
-    config.database.name,
-    config.database.user,
-    config.database.password,
     {
-        host: config.database.host,
-        dialect: config.database.dialect
+        development: {
+            database: {
+                host: process.env.DB_HOST,
+                port: process.env.DB_PORT,
+                name: process.env.DB_NAME,
+                dialect: process.env.DB_DIALECT,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD
+            }
+        },
+        production: {
+            database: {
+                host: process.env.DB_HOST,
+                port: process.env.DB_PORT,
+                name: process.env.DB_NAME,
+                dialect: process.env.DB_DIALECT,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD
+            }
+        }
     }
 )
 
